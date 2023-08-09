3bec2f0f25699594b2cc0e494a282174cb0433f1
SUDHARSHAN0712 <139960958+SUDHARSHAN0712@users.noreply.github.com>
2023-08-09 10:48:17 +0530

    Add files via upload

---
+++ src/StudentDB.tsx
@@ -0,0 +1,23 @@
+export const student = [{
+    name : "B.Sudharshan",
+    regno : 20110112,
+    designation : "Manager"
+},{
+    name : "C.Logesh",
+    regno : 20110008,
+    designation : "Web developer"
+},{
+    name : "V.Sanjay",
+    regno : 20110080,
+    designation : "Team Leader"
+},{
+    name : "Subash Kumar",
+    regno : 20110109,
+    designation : "Web developer"
+},{
+    name : "Udhay Kumar",
+    regno : 20104059,
+    designation : "Web developer"
+},
+
+]
